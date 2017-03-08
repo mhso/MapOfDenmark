8bc77b3880fee02f66fd6487d961b725d93434b3
magnus <mjac@itu.dk>
2017-03-09 00:45:13 +0100

    darktheme

--- src/dk/itu/n/danmarkskort/GUIManager.java
+++ src/dk/itu/n/danmarkskort/GUIManager.java
@@ -1,15 +1,19 @@
 package dk.itu.n.danmarkskort;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class GUIManager extends JPanel{
 
     public final static int
-            TOP_PANEL_WIDTH = 400,
+            TOP_PANEL_WIDTH = 450,
             TOP_PANEL_HEIGHT = 50,
             STANDARD_MARGIN = 40,
             SMALL_MARGIN = 10;
+
+    private final Color panelBorderColor = new Color(0, 0, 0),
+                        panelBackground = new Color(60, 70, 80, 255);
 
     public GUIManager() {
         setBounds(0, 0, MainPanel.WIDTH, MainPanel.HEIGHT);
@@ -19,12 +23,13 @@ public class GUIManager extends JPanel{
 
     public void addTopPanel() {
         JPanel topPanel = new JPanel();
-        topPanel.setBackground(new Color(255, 255, 255, 70));
+        topPanel.setBackground(panelBackground);
         topPanel.setPreferredSize(new Dimension(TOP_PANEL_WIDTH, TOP_PANEL_HEIGHT));
-        topPanel.setBounds((MainPanel.WIDTH / 2) - (TOP_PANEL_WIDTH / 2),
+        /*topPanel.setBounds((MainPanel.WIDTH / 2) - (TOP_PANEL_WIDTH / 2),
                 STANDARD_MARGIN,
                 TOP_PANEL_WIDTH,
-                TOP_PANEL_HEIGHT);
+                TOP_PANEL_HEIGHT);*/
+        topPanel.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, panelBorderColor));
         add(topPanel);
     }
 
