35f502f61c718bfe8d364ff1ae03bcb8d4034159
wituz <wituz@localhost.localdomain>
2017-03-09 21:20:47 +0100

    XML Parses delvist

--- src/dk/itu/n/danmarkskort/Main.java
+++ src/dk/itu/n/danmarkskort/Main.java
@@ -1,23 +1,29 @@
 package dk.itu.n.danmarkskort;
 
+import dk.itu.n.danmarkskort.backend.OSMParser;
+import dk.itu.n.danmarkskort.backend.TileController;
+
 public class Main {
 
 	public final static String APP_NAME = "FrankMaps";
 	public final static String APP_VERSION = "0.1";
+	public final static boolean debug = true;
+	public final static boolean production = false;
+	public static OSMParser osmParser;
+	public static TileController tileController;
 	
 	public static void main(String[] args) {
-		startup();
+		startup(args);
+		Main.log("Application succesfully started.");
 		main();
 		shutdown();
+		Main.log("Application ended.");
 	}
-	
-	
-	public static void log(String text) {
-		System.out.println("[" + APP_NAME + " " + APP_VERSION + "] " + text);
-	}
-	
-	public static void startup() {
-		log("Application started.");
+		
+	public static void startup(String[] args) {
+		if(args.length == 1) osmParser = new OSMParser(args[0]);
+		else osmParser = new OSMParser();
+		tileController = new TileController();
 	}
 	
 	public static void main() {
@@ -25,8 +31,16 @@ public class Main {
 	}
 	
 	public static void shutdown() {
-		log("Application ended.");
+		
 	}
 
+	public static void log(String text) {
+		if(debug) System.out.println("[" + APP_NAME + " " + APP_VERSION + "] " + text);
+	}
+	
+	public static void logRamUsage() {
+		float usage = Util.getRAMUsageInMB();
+		log("RAM usage: " + usage + "MB");
+	}
 	
 }
