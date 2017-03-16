2060811d829f3bd9ab0be8c9a7a4a433cf6b7e77
Frank Andersen <fand@itu.dk>
2017-03-16 11:11:34 +0100

    Merge conflict

--- src/dk/itu/n/danmarkskort/gui/GUIManager.java
+++ src/dk/itu/n/danmarkskort/gui/GUIManager.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package dk.itu.n.danmarkskort.gui;
 
 import javax.swing.*;
@@ -25,31 +24,3 @@ public class GUIManager extends JPanel{
         setOpaque(false);
     }
 }
-=======
-package dk.itu.n.danmarkskort.gui;
-
-import javax.swing.*;
-import java.awt.*;
-
-public class GUIManager extends JPanel{
-
-    Style style;
-
-    public GUIManager() {
-
-        // manages the current stylesheet
-        style = new Style();
-
-        setLayout(new BorderLayout());
-
-        // margin for the whole frame
-        setBorder(BorderFactory.createEmptyBorder(style.margin(), style.margin(), style.margin(), style.margin()));
-
-        add(new TopPanel(style), BorderLayout.NORTH);
-
-        add(new BottomPanel(style), BorderLayout.SOUTH);
-
-        setOpaque(false);
-    }
-}
->>>>>>> branch 'develop' of https://github.itu.dk/fand/BFST17_DK_Kort
