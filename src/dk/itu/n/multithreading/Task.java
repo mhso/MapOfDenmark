f26fb0182891e23a6037fa143647ee8aa30930f3
Frederik Tejner Witte <wituz@debian>
2017-04-20 15:16:56 +0200

    Multithreading queue system!

---
+++ src/dk/itu/n/multithreading/Task.java
@@ -0,0 +1,40 @@
+package dk.itu.n.multithreading;
+
+public abstract class Task implements Runnable{
+	
+	private boolean isRunning = true;
+	private TaskPriority priority = TaskPriority.MEDIUM;
+	
+	public Task() {
+		priority = TaskPriority.MEDIUM;
+	}
+	
+	public Task(TaskPriority priority) {
+		this.priority = priority;
+	}
+	
+	public void run() {
+		isRunning = true;
+		onRunStart();
+		work();
+		onRunEnd();
+		isRunning = false;
+	}
+	
+	public boolean isRunning() {
+		return isRunning;
+	}
+	
+	public TaskPriority getPriority() {
+		return priority;
+	}
+	
+	public void setPriority(TaskPriority priority) {
+		this.priority = priority;
+	}
+	
+	public abstract void work();
+	public abstract void onRunStart();
+	public abstract void onRunEnd();
+	
+}
