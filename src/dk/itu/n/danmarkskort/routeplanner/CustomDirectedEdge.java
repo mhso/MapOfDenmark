6efff750fd2ee9151cf615a2360e773cbe1e87ad
Frank Andersen <fand@itu.dk>
2017-04-20 15:27:38 +0200

    Startet on routeplanner algro.

---
+++ src/dk/itu/n/danmarkskort/routeplanner/CustomDirectedEdge.java
@@ -0,0 +1,7 @@
+package dk.itu.n.danmarkskort.routeplanner;
+
+public class CustomDirectedEdge {
+	private final float lon, lat;
+	private final double weight;
+
+}
---
+++ src/dk/itu/n/danmarkskort/routeplanner/CustomGraph.java
@@ -0,0 +1,11 @@
+package dk.itu.n.danmarkskort.routeplanner;
+
+public class CustomGraph {
+	private int numOfVertices;
+	private int numOfEdges;
+	
+	public CustomGraph(){
+		
+	}
+}
+
