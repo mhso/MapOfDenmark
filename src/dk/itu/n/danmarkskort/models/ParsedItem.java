3f67186fdcc891fce1ff65012ffa9402f1131140
Bruger <Bruger@LAPTOP-03QT7N5V>
2017-04-27 17:11:34 +0200

    Fixed weird thing (maybe)

--- src/dk/itu/n/danmarkskort/models/ParsedItem.java
+++ src/dk/itu/n/danmarkskort/models/ParsedItem.java
@@ -10,16 +10,6 @@ public abstract class ParsedItem implements KDComparable, Serializable {
 
 	private static final long serialVersionUID = -8684498422989385773L;
 
-    @Override
-    public int compareLat(KDComparable item) {
-        float a = getFirstNode().getLat();
-        float b = item.getFirstNode().getLat();
-        if(a > b) return 1;
-        if(a == b) return 0;
-        return 0;
-    }
-    
-    public abstract ParsedNode[] getNodes();
     public abstract Shape getShape();
     public abstract void deleteOldRefs();
     public abstract void nodesToCoords();
