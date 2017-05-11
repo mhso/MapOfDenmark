d74789f6c2cd86b2cb496daa6d3cdeeeec584035
Guellen <Guellen@10.26.38.46>
2017-05-11 10:14:50 +0200

    Added thanks to Stackoverflow for JAR-entry-stream.

--- src/dk/itu/n/danmarkskort/gui/menu/AboutUsPage.java
+++ src/dk/itu/n/danmarkskort/gui/menu/AboutUsPage.java
@@ -123,7 +123,9 @@ public class AboutUsPage extends JPanel  {
         		+ "<br>  Levehnstein implementation as well as inspiration for "
         		+ "<br>  our loading screen from their CountingInputStream."
         		+ "<br>- Darryl Burke for his AlphaImageIcon implementation:"
-        		+ "<br> https://tips4java.wordpress.com/2010/08/22/alpha-icons.";
+        		+ "<br>  https://tips4java.wordpress.com/2010/08/22/alpha-icons."
+        		+ "<br>- 'user1079877' from Stackoverflow for his JAR entries"
+        		+ "<br>  stream.";
         
         JLabel lblAppDescription = new JLabel(appDescription);
         GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
