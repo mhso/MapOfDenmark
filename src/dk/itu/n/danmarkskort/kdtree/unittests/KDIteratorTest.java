6fece5d5c28b4b93b12aa3dbe33c472f58d68c4a
magnus <mjac@itu.dk>
2017-05-13 15:37:38 +0200

    tinkered some more with kd tests

--- src/dk/itu/n/danmarkskort/kdtree/unittests/KDIteratorTest.java
+++ src/dk/itu/n/danmarkskort/kdtree/unittests/KDIteratorTest.java
@@ -8,6 +8,8 @@ import dk.itu.n.danmarkskort.kdtree.KDTreeNode;
 import dk.itu.n.danmarkskort.models.ParsedWay;
 
 import dk.itu.n.danmarkskort.models.Region;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.awt.geom.Point2D;
@@ -15,6 +17,38 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class KDIteratorTest {
+
+    Region surrounding;
+    Region surroundingReversed;
+    Region surrounded;
+    Region alwaysOutsideNegative;
+    Region alwaysOutsidePositive;
+    Region oneToTwentyFive;
+    Region twentySevenToFifty;
+    Region oneToFiveHundred;
+    Region oneToFiftyThousand;
+    Region negativeCloseToZero;
+
+    KDTree<KDComparable> tree;
+
+    @Before
+    public void setUp() {
+        surrounding         = new Region(-1, -1, 1000000, 1000000);
+        surroundingReversed = new Region(1000000, 1000000, -1, -1);
+        surrounded          = new Region(1, 1, 1, 1);
+        alwaysOutsideNegative = new Region(-100000, -100000, -10000, -10000);
+        alwaysOutsidePositive = new Region(1000000, 1000000, 100000, 100000);
+        oneToTwentyFive     = new Region(1, 1, 25, 25);
+        twentySevenToFifty  = new Region(27, 27, 50, 50);
+        oneToFiveHundred    = new Region(1, 1, 500, 500);
+        oneToFiftyThousand  = new Region(1, 1, 50000, 50000);
+        negativeCloseToZero = new Region(-2, -2, -1, -1);
+    }
+
+    @After
+    public void tearDown() {
+        tree = null;
+    }
 
     public KDTreeNode<KDComparable> makeKDTree(int dataSize, int coordsPerElement, float startLon, float startLat, int leafSize) {
         ArrayList<ParsedWay> list = new ArrayList<>();
@@ -43,53 +77,150 @@ public class KDIteratorTest {
         return count;
     }
 
-    @Test
-    public void testWithRegions() {
-        KDTree<KDComparable> oneElement =
-                makeKDTree(1, 1, 1, 1, 25);
-        KDTree<KDComparable> twoElements =
-                makeKDTree(2, 1, 1, 1, 25);
-        KDTree<KDComparable> fiftyElements =
-                makeKDTree(50, 1, 1, 1, 50);
-        KDTree<KDComparable> manyElements =
-                makeKDTree(1000, 1, 1, 1, 25);
-        KDTree<KDComparable> ExtremeElements =
-                makeKDTree(100000, 1, 1, 1,25);
-
-        Region surrounding = new Region(-1, -1, 100001, 100001);
-        Region surroundingReversed = new Region(100001, 100001, -1, -1);
-        Region surrounded = new Region(1, 1, 1, 1);
-        Region alwaysOutside = new Region(-1000, -1000, -100, -100);
-        Region alwaysOutside2 = new Region(1000000, 1000000, 1000001, 1000001);
-        Region halfOfFifty = new Region(1, 1, 24, 24);
-        Region halfOfMany = new Region(1, 1, 499, 499);
-        Region halfOfExtreme = new Region(1, 1, 49999, 49999);
-
-        // tests on tree with one element
-        assertEquals(1, nextCount(oneElement, surrounding));
-        assertEquals(1, nextCount(oneElement, surroundingReversed));
-        assertEquals(1, nextCount(oneElement, surrounded));
-        assertEquals(0, nextCount(oneElement, alwaysOutside));
-        assertEquals(0, nextCount(oneElement, alwaysOutside2));
-        assertEquals(1, nextCount(oneElement, halfOfExtreme));
-
-        // tests on tree with two elements
-        assertEquals(2, nextCount(twoElements, surrounding));
-        assertEquals(1, nextCount(twoElements, surrounded));
-        assertEquals(0, nextCount(twoElements, alwaysOutside));
-
-        // tests on tree with fifty elements
-        assertEquals(50, nextCount(fiftyElements, surrounding));
-        assertEquals(25, nextCount(fiftyElements, surrounded));
-        assertEquals(50, nextCount(fiftyElements, surroundingReversed));
-        assertEquals(0, nextCount(fiftyElements, alwaysOutside));
-        assertEquals(25, nextCount(fiftyElements, halfOfFifty));
-
+    public KDComparable getElement(KDTree<KDComparable> tree, Region region) {
+        KDComparable element;
+        for(Iterator<KDComparable> iter = tree.iterator(region); iter.hasNext();) return iter.next();
+        return null;
     }
-/*
+
+    public int nextCountWithoutRegion(KDTree<KDComparable> tree) {
+        return nextCount(tree, null);
+    }
+
     @Test
-    public void testWithoutRegions() {
+    public void testOneElement() {
+        int size = 1;
+        tree = makeKDTree(size, 1, 1, 1, 25);
+        assertEquals(size, nextCount(tree, surrounding));
+        assertEquals(size, nextCount(tree, surroundingReversed));
+        assertEquals(size, nextCount(tree, surrounded));
+        assertEquals(0, nextCount(tree, alwaysOutsideNegative));
+        assertEquals(0, nextCount(tree, alwaysOutsidePositive));
+        assertEquals(size, nextCount(tree, oneToFiftyThousand));
+        assertEquals(size, nextCountWithoutRegion(tree));
+    }
 
-    }*/
+    @Test
+    public void testTwoElements() {
+        int size = 2;
+        tree = makeKDTree(size, 1, 1, 1, 25);
+        assertEquals(size, nextCount(tree, surrounding));
+        assertEquals(size, nextCount(tree, surrounded));
+        assertEquals(0, nextCount(tree, alwaysOutsideNegative));
+        assertEquals(size, nextCountWithoutRegion(tree));
+    }
 
+    @Test
+    public void testUnevenSmallAmountElements() {
+        int size = 27;
+        tree = makeKDTree(size, 1, 1, 1, 25);
+        assertEquals((size + 1) / 2, nextCount(tree, surrounded));
+        assertEquals(size, nextCount(tree, surrounding));
+        assertEquals(size,  nextCount(tree, oneToTwentyFive));
+        assertEquals(size / 2, nextCount(tree, twentySevenToFifty));
+        assertEquals(0,  nextCount(tree, alwaysOutsidePositive));
+        assertEquals(size, nextCountWithoutRegion(tree));
+    }
+
+    @Test
+    public void testUnevenLargerAmountElements() {
+        int size = 51;
+        tree = makeKDTree(size, 1, 1, 1, 25);
+        assertEquals((size + 1) / 4, nextCount(tree, surrounded));
+        assertEquals((size + 1) / 2, nextCount(tree, oneToTwentyFive));
+        assertEquals(size / 2, nextCount(tree, twentySevenToFifty));
+        assertEquals(size, nextCountWithoutRegion(tree));
+    }
+
+    @Test
+    public void testFiftyElements() {
+        int size = 50;
+        tree = makeKDTree(size, 1, 1, 1, 25);
+        assertEquals(size, nextCount(tree, surrounding));
+        assertEquals(size, nextCount(tree, surroundingReversed));
+        assertEquals((size + 1) / 2, nextCount(tree, surrounded));
+        assertEquals(0,  nextCount(tree, alwaysOutsidePositive));
+        assertEquals(size / 2, nextCount(tree, oneToTwentyFive));
+        assertEquals(size, nextCountWithoutRegion(tree));
+    }
+
+    @Test
+    public void testEightHundredElements() {
+        int size = 800;
+        tree = makeKDTree(size, 1, 1, 1, 25);
+        assertEquals(25, nextCount(tree, twentySevenToFifty));
+        assertEquals(500, nextCount(tree, oneToFiveHundred));
+    }
+
+    @Test
+    public void testOneThousandElements() {
+        int size = 1000;
+        tree = makeKDTree(size, 1, 1, 1, 25);
+        assertNotEquals(25, nextCount(tree, surrounded)); // halved recursively, 1000 does not end with uniform numbers of 25
+        assertTrue(nextCount(tree, surrounded) < 25);
+        assertTrue(nextCount(tree, surrounded) > 0);
+        assertEquals(1000, nextCount(tree, surroundingReversed));
+        assertEquals(0, nextCount(tree, alwaysOutsideNegative));
+        assertEquals(500, nextCount(tree, oneToFiveHundred)); // halved once, 1000 still gives equal numbers
+        assertEquals(1000, nextCountWithoutRegion(tree));
+    }
+
+    @Test
+    public void testNegativeCoords() {
+        int size = 1000;
+        tree = makeKDTree(size, 1, -1000, -1000, 25);
+        assertEquals(0, nextCount(tree, surrounded));
+        assertEquals(0, nextCount(tree, alwaysOutsideNegative));
+        assertTrue(nextCount(tree, negativeCloseToZero) <= 25 && nextCount(tree, negativeCloseToZero) > 0);
+        assertEquals(size, nextCountWithoutRegion(tree));
+    }
+
+    @Test
+    public void testOneHundredThousandElements() {
+        int size = 100__000;
+        tree = makeKDTree(size, 1, 1, 1,25);
+        assertEquals(25, nextCount(tree, surrounded));
+        assertEquals(size, nextCount(tree, surrounding));
+        assertEquals(size, nextCount(tree, surroundingReversed));
+        assertEquals(size / 2, nextCount(tree, oneToFiftyThousand));
+        assertEquals(size, nextCountWithoutRegion(tree));
+    }
+
+    @Test
+    public void testLargerLeafSize() {
+        int size = 1000;
+        int leafSize = 500;
+        tree = makeKDTree(size, 1, 1, 1, leafSize);
+        assertEquals(size / 2, nextCount(tree, surrounded));
+        assertEquals(0, nextCount(tree, alwaysOutsideNegative));
+        assertEquals(0, nextCount(tree, alwaysOutsidePositive));
+        assertEquals(size, nextCountWithoutRegion(tree));
+    }
+
+    @Test
+    public void testMinimumLeafSize() {
+        int size = 1000;
+        int leafSize = 1;
+        tree = makeKDTree(size, 1, 1, 1, leafSize);
+        assertEquals(leafSize, nextCount(tree, surrounded));
+        assertEquals(size, nextCountWithoutRegion(tree));
+
+        Point2D.Float found = getElement(tree, surrounded).getFirstNode();
+        Point2D.Float expected = new Point2D.Float(1, 1);
+        Point2D.Float notExpected = new Point2D.Float(2, 2);
+        assertEquals(expected, found);
+        assertNotEquals(notExpected, found);
+    }
+
+    @Test
+    public void testManyCoordsPerElement() {
+        int size = 100;
+        int coordSize = 100;
+        tree = makeKDTree(size, coordSize, 1, 1, 25);
+        assertEquals(size / 2, nextCount(tree, twentySevenToFifty));
+        assertEquals(size / 4, nextCount(tree, surrounded));
+        assertEquals(0, nextCount(tree, alwaysOutsideNegative));
+        assertEquals(0, nextCount(tree, alwaysOutsidePositive));
+        assertEquals(size, nextCountWithoutRegion(tree));
+    }
 }
--- src/dk/itu/n/danmarkskort/kdtree/unittests/NearestNeighborTest.java
+++ src/dk/itu/n/danmarkskort/kdtree/unittests/NearestNeighborTest.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class NearestNeighborTest {
 
 	public KDTreeNode<KDComparable> makeKDTreeWithWays(int dataSize, int coordsPerElement, float startLon, float startLat, int leafSize) {
-		ArrayList<ParsedWay> list = new ArrayList<>();
+		List<ParsedWay> list = new ArrayList<>();
 		for(int i = 0; i < dataSize; i++) {
 			ParsedWay way = new ParsedWay(i);
 			Point2D.Float[] nodes = new Point2D.Float[coordsPerElement];
@@ -30,7 +30,7 @@ public class NearestNeighborTest {
 		return new KDTreeNode(list, leafSize);
 	}
 
-	public KDTree<KDComparable> makeKDTreeWithHousenumbers(int dataSize, float lon, float lat, int leafSize) {
+	public KDTree<KDComparable> makeKDTreeWithPoints(int dataSize, float lon, float lat, int leafSize) {
 		List<Housenumber> items = new ArrayList<>();
 		for(int i = 0; i < dataSize; i++) {
 			Housenumber housenumber = new Housenumber(null, null, null, new Point2D.Float(lon+i, lat+i));
@@ -39,43 +39,62 @@ public class NearestNeighborTest {
 		return new KDTreeNode(items, leafSize);
 	}
 
-	@Test
-	public void testPositiveValues() {
-		KDTree<KDComparable> tree = makeKDTreeWithHousenumbers(1000, 1, 1, 25);
-		Point2D.Float target = new Point2D.Float(1, 1);
-		Point2D.Float target2 = new Point2D.Float(2, 2);
-		Point2D.Float target3 = new Point2D.Float(500, 500);
+    @Test
+    public void testNullQuery() {
+        KDTree<KDComparable> tree = makeKDTreeWithPoints(1000, 1, 1, 25);
+        assertNull(tree.nearest(null));
+    }
 
-		assertEquals(target, tree.nearest(target).getFirstNode());
+	@Test
+	public void testWithPoints() {
+		KDTree<KDComparable> tree = makeKDTreeWithPoints(1000, 1, 1, 25);
+		Point2D.Float target1 = new Point2D.Float(1, 1);
+		Point2D.Float query1 = target1;
+		Point2D.Float target2 = new Point2D.Float(2, 2);
+		Point2D.Float query2 = target2;
+		Point2D.Float target3 = new Point2D.Float(500, 500);
+		Point2D.Float query3 = target3;
+		Point2D.Float target4 = new Point2D.Float(1000, 1000);
+		Point2D.Float query4 = new Point2D.Float(2000, 2000);
+        Point2D.Float query5 = new Point2D.Float(-10, -10);
+
+        assertEquals(target1, tree.nearest(query1).getFirstNode());
+		assertEquals(target1, tree.nearest(query5).getFirstNode());
 		assertEquals(target2, tree.nearest(target2).getFirstNode());
 		assertEquals(target3, tree.nearest(target3).getFirstNode());
-		assertNotEquals(target3, tree.nearest(target).getFirstNode());
+		assertEquals(target4, tree.nearest(query4).getFirstNode());
+		assertNotEquals(target3, tree.nearest(query4).getFirstNode());
+		assertNotEquals(target3, tree.nearest(query1).getFirstNode());
 	}
 
 	@Test
-	public void testOnTreeWithLargeLeafs() {
-		KDTree<KDComparable> tree = makeKDTreeWithHousenumbers(1000, 1, 1, 1000);
-		Point2D.Float target = new Point2D.Float(1, 1);
-		Point2D.Float target2 = new Point2D.Float(2, 2);
-		Point2D.Float target3 = new Point2D.Float(500, 500);
+	public void testWithPointsAndLargeLeafs() {
+		KDTree<KDComparable> tree = makeKDTreeWithPoints(1000, 1, 1, 1000);
+		Point2D.Float target1 = new Point2D.Float(1, 1);
+		Point2D.Float query1 = target1;
+        Point2D.Float target2 = new Point2D.Float(1000, 1000);
+        Point2D.Float query2 = new Point2D.Float(2000, 2000);
 
-		assertEquals(target, tree.nearest(target).getFirstNode());
-		assertEquals(target2, tree.nearest(target2).getFirstNode());
-		assertEquals(target3, tree.nearest(target3).getFirstNode());
-		assertNotEquals(target3, tree.nearest(target).getFirstNode());
+		assertEquals(target1, tree.nearest(query1).getFirstNode());
+        assertEquals(target2, tree.nearest(query2).getFirstNode());
+        assertNotEquals(target1, tree.nearest(query2).getFirstNode());
 	}
 
     @Test
-    public void testNegativeValues() {
-        KDTree<KDComparable> tree = makeKDTreeWithHousenumbers(1000, -1000, -1000, 25);
-        Point2D.Float target = new Point2D.Float(-1, -1);
-        Point2D.Float target2 = new Point2D.Float(-2, -2);
-        Point2D.Float target3 = new Point2D.Float(-500, -500);
+    public void testWithPointsNegativeValues() {
+        KDTree<KDComparable> tree = makeKDTreeWithPoints(1000, -1000, -1000, 25);
+        Point2D.Float target1 = new Point2D.Float(-1, -1);
+        Point2D.Float query1 = target1;
+        Point2D.Float target2 = new Point2D.Float(-1000, -1000);
+        Point2D.Float query2 = new Point2D.Float(-2000, -2000);
 
-        assertEquals(target, tree.nearest(target).getFirstNode());
-        assertEquals(target2, tree.nearest(target2).getFirstNode());
-        assertEquals(target3, tree.nearest(target3).getFirstNode());
-        assertNotEquals(target3, tree.nearest(target).getFirstNode());
+        assertEquals(target1, tree.nearest(query1).getFirstNode());
+        assertEquals(target2, tree.nearest(query2).getFirstNode());
+        assertNotEquals(target1, tree.nearest(query2).getFirstNode());
+    }
+    @Test
+    public void testWithWays() {
+
     }
 
 	/*@Test
