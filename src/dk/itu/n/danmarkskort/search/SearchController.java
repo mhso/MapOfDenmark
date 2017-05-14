3ce1da12b54cfe8eacbcbfc0e31ca7482999fd9e
Frank Andersen <fand@itu.dk>
2017-05-14 22:00:09 +0200

    Java doc.

--- src/dk/itu/n/danmarkskort/search/SearchController.java
+++ src/dk/itu/n/danmarkskort/search/SearchController.java
@@ -7,8 +7,14 @@ import dk.itu.n.danmarkskort.Util;
 import dk.itu.n.danmarkskort.address.Address;
 
 public class SearchController{
-	private SearchController(){ }
 	
+	/**
+	 * Get search suggestions by input, text can return multiple results,
+	 * while coordinates can return nearst address as suggestion.
+	 * 
+	 * @param inputStr address string or coordinates
+	 * @return an list of results
+	 */
 	public static List<String> getSearchFieldSuggestions(String inputStr){
 		long limitAmountOfResults = 10;
 		
@@ -21,6 +27,12 @@ public class SearchController{
 		}
 	}
 	
+	/**
+	 * If search return a single address, return match.
+	 * 
+	 * @param inputStr address string or coordinates
+	 * @return an address object or null
+	 */
 	public static Address getSearchFieldAddressObj(String inputStr){
 		if(inputStr == null || inputStr.isEmpty()) return null;
 		
@@ -31,6 +43,11 @@ public class SearchController{
 		}
 	}
 	
+	/**
+	 * Checks if input is coordinates.
+	 * @param inputStr a string
+	 * @return true if is coordinates.
+	 */
 	private static boolean isCoordinates(String inputStr){
 		String cordRegex = "((\\-{0,1})([0-9]{1,3})(\\.)(\\-{0,1})([0-9]{5,9}))";
 		String cordsRegex = cordRegex + "(\\,\\s)" + cordRegex;
