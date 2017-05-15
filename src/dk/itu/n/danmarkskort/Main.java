4f0513e1df2f34acc6e2ff12e270ac1417a30040
Frederik Tejner Witte <wituz@debian>
2017-05-15 19:44:05 +0200

    Debug set back to false

--- src/dk/itu/n/danmarkskort/Main.java
+++ src/dk/itu/n/danmarkskort/Main.java
@@ -24,7 +24,7 @@ public class Main {
 	
 	public final static String APP_NAME = "Yak Maps";
 	public final static String APP_VERSION = "0.9";
-	public final static boolean debug = false;
+	public final static boolean debug = true;
 	public final static boolean debugExtra = false;
 	public final static boolean production = false;
 	public final static boolean buffered = true;
